b324024f125818e9fba525371e2b6178c4f9db76
sergio-av <sergioavilachacon@gmail.com>
2021-03-23 13:41:50 +0100

    Conection whit firebase done, Anon Auth done

---
+++ flutter_app/lib/screens/wrappe.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/authenticate/authenticate.dart';
+import 'package:flutter_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Authenticate();
+  }
+}
